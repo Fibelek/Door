fac20959479dc7de6f42925a4706461aa0835f77
Fibelek <131624031+Fibelek@users.noreply.github.com>
2023-10-04 10:21:56 +0300

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,31 @@
+]#include <iostream>
+
+int main()
+{
+cout<< "Hello world from Daniil!"<< endl
+}
+EOF
+
+
+
+
+[A[A[A[A[A[B[A[C[C[A[C[C[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C << EOF[B[B[B[B[B[B[D[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[DEOF
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[A[A[A[A[A[Agit status
+git
+help
+
